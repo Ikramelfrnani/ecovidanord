948b893d3c02fc3fc7b88c4f674058334d138db3
Ikram Elfrnani <ikram.elfrnani@gmail.com>
2025-11-06 20:26:45 +0100

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Services from './Pages/Services';
 import DetailsServices from './Pages/DetailsServices'; 
 import AboutUs from "./Pages/AboutUs"; 
